fb07ea74e57bbc84217b93ceecded1bf8e3eedb2
juanegido <Egido.2020>
2022-09-25 19:21:46 +0200

    Use middleware log

--- internal/platform/server/middleware/recovery/recovery.go
+++ internal/platform/server/middleware/recovery/recovery.go
@@ -1,14 +1,17 @@
 package recovery
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 func Middleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
 			if err := recover(); err != nil {
+				fmt.Printf("[Middleware] %s panic recovered:\n%s\n", time.Now().Format("2006/01/02 - 15:04:05"), err)
 				c.Abort()
 				c.AbortWithStatus(http.StatusInternalServerError)
 			}
